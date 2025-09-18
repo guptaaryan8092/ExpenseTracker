3f9bda86cf33fc0eb050ce0fb550caf6113785e2
guptaaryan8092 <guptaaryan8092@gmail.com>
2025-09-18 23:33:16 +0530

    .

--- Server/models/Transaction.js
+++ Server/models/Transaction.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 const transactionSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },
   type: { type: String, enum: ['income', 'expense'], required: true },
   category: { type: String, required: true },
   amount: { type: Number, required: true },
